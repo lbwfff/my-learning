2d359226abd11c2e366adde10671089c5dd65388
LeeLee <74947198+lbwfff@users.noreply.github.com>
2023-02-14 14:46:59 +0800

    Update ChIPpeakAnno.R

--- ChIPpeakAnno.R
+++ ChIPpeakAnno.R
@@ -51,3 +51,56 @@ makeVennDiagram(ol,fill=c(met.brewer('Hiroshige',n=4)[c(1,3)]),
                 col=c("#D55E00", "#009E73"), ignore.strand=F,
                 cat.col=c("#D55E00", "#0072B2"),cat.cex =1.2)
 
+####################################################################################################
+#当我们做RIP-seq时我们会比较好奇peak在转录本上的未知信息
+
+library(ChIPpeakAnno)
+
+annoData <- toGRanges('../gencode/gencode.v43.basic.annotation.gff3',format=c('GFF'))##自己的注释文件
+cel_feature<-c('gene','CDS','five_prime_UTR','three_prime_UTR','stop_codon') #我只想要这几个特征
+
+annoData@ranges@NAMES<-paste0(annoData@elementMetadata@listData$gene_id,'-',annoData@elementMetadata@listData$type)
+annoData<-annoData[annoData@elementMetadata@listData$type %in% cel_feature,]
+table(annoData@elementMetadata@listData[["type"]])
+annoframe<-as.data.frame(annoData,row.names = 1:length(annoData@elementMetadata@listData$gene_id))
+annoframe$adj_id<-paste0(annoframe$gene_id,'-',annoframe$type)
+
+annoData@elementMetadata@listData$type<-factor(rep('gene',length(annoData@elementMetadata@listData$type)))
+#想的是对软件做一个欺骗，然后可以得到想要的特征信息
+
+filelist<-c('T1','M2','NC')
+
+for (i in filelist){
+  print(i)
+  path<-paste0(i,'.bed')
+  peaks <- read.table(path,sep = '\t')
+  path<-paste0(i,'_adj.bed')
+  write.table(peaks[,1:6],file = path,row.names = F,col.names = F,quote = F,sep = '\t')
+  peaks <- toGRanges(path, format="BED")
+  seqlevelsStyle(peaks) <- seqlevelsStyle(annoData)
+
+  anno <- annotatePeakInBatch(peaks, AnnotationData=annoData,
+                            output="overlapping",ignore.strand = FALSE)
+  
+  anno<-anno[!duplicated(anno@elementMetadata@listData$feature)]
+  anno<-anno[!is.na(anno@elementMetadata@listData$feature),]
+  anno<-as.data.frame(anno)
+
+  matchanno<-annoframe[match(anno$feature,annoframe$adj_id),]
+  anno$gene_type<-matchanno$gene_type
+  anno$gene_name<-matchanno$gene_name
+
+  f <-function(x) unlist(strsplit(x['feature'],'-'))[2]
+  anno$feartre_type<-apply(anno,1,f)
+  
+  anno<-anno[!duplicated(paste0(anno$peak,'_',anno$gene_name,'_',anno$feartre_type)),]
+  
+  table(anno$feartre_type)
+  table(anno$gene_type)
+
+  path<-paste0(i,'ann.csv')
+  write.csv(anno,file = path)
+  }
+
+
+
