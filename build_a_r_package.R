66bef2b5fbe6b3fc03df2b1a720b0edc0c254215
LeeLee <74947198+lbwfff@users.noreply.github.com>
2024-03-14 13:43:42 +0400

    Create build_a_r_package.R

---
+++ build_a_r_package.R
@@ -0,0 +1,10 @@
+
+# 检查包
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+#
+# roxygen2::roxygenize()  可以自动生成rd文件
+# usethis::use_package("") 添加依赖
+# rm(list= ls())
